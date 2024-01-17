166ae818f4b2bba4a8e1a464c7615eb868122d39
Neralb Cika <neralbcika124@gmail.com>
2024-01-17 17:49:33 +0100

    zm zs

--- entities/client.py
+++ entities/client.py
@@ -57,14 +57,14 @@ class Client:
         print(x.shape)
         self.model=self.model.cuda()
         _,features = self.model(x)
-        print('printing features')
-        print(features.shape)
         z_mu = features[:,:int(self.z_dim/2)]
         z_sigma = F.softplus(features[:,int(self.z_dim/2):])
         z_mu = z_mu.to(x.device)
+        print('printing z_mu and z_sigma')
+        print(z_mu.shape , z_sigma.shape)
         z_sigma = z_sigma.to(x.device)
         z_dist = distributions.Independent(distributions.normal.Normal(z_mu,z_sigma),1)
-        z = z_dist.rsample([num_samples]).view([num_samples, -1, self.z_dim])
+        z = z_dist.rsample([num_samples]).view([-1,self.z_dim])
         print('Im in featurize 2')
         print(z.shape)
         if return_dist:
